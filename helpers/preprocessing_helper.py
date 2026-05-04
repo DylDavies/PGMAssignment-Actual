b6a11acfe9b43bb8e1fc49fb4009a955fff9b225
Dylan Davies <34412943+DylDavies@users.noreply.github.com>
2026-05-04 20:28:17 +0200

    Fix pre processing issues

--- helpers/preprocessing_helper.py
+++ helpers/preprocessing_helper.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import KBinsDiscretizer, MinMaxScaler
+from sklearn.preprocessing import KBinsDiscretizer, StandardScaler
 
 import config
 
@@ -14,26 +14,35 @@ for stage in config.CONTINUOUS:
 
 def impute(
     df: pd.DataFrame
-) -> None:
+) -> tuple[SimpleImputer, SimpleImputer]:
     # Continuous imputing
     continuous_imputer = SimpleImputer(strategy="median")
     df[continuous_cols] = continuous_imputer.fit_transform(df[continuous_cols])
-
     
     # Discrete Imputing
     discrete_imputer = SimpleImputer(strategy="most_frequent")
     df[discrete_cols] = discrete_imputer.fit_transform(df[discrete_cols])
 
+    return continuous_imputer, discrete_imputer
+
+def apply_imputer(
+    df: pd.DataFrame,
+    continuous_imputer: SimpleImputer,
+    discrete_imputer: SimpleImputer
+) -> None:
+    df[continuous_cols] = continuous_imputer.transform(df[continuous_cols]) # type: ignore
+    df[discrete_cols] = discrete_imputer.transform(df[discrete_cols]) # type: ignore
+
 def scale(
     df: pd.DataFrame
-) -> MinMaxScaler:
-    sc = MinMaxScaler()
+) -> StandardScaler:
+    sc = StandardScaler()
     df[continuous_cols] = sc.fit_transform(df[continuous_cols])
 
     return sc
 
 def apply_scaler(
     df: pd.DataFrame,
-    scaler: MinMaxScaler
+    scaler: StandardScaler
 ) -> None:
     df[continuous_cols] = scaler.transform(df[continuous_cols])
---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+from pathlib import Path
+
+from config import ROOT_DIR
+from helpers.data_helper import download_dataset, load_merged, split_train_test
+from helpers.preprocessing_helper import impute, apply_imputer, scale, apply_scaler
+
+import config
+
+# Make sure dataset is downloaded and present
+download_dataset()
+
+# Load merged DF
+df = load_merged()
+
+# Create training and test split
+train_df, test_df = split_train_test(df)
+normal_train = train_df[train_df[config.LABEL_COL] == config.NORMAL_LABEL].copy()
+
+print(f"\nTrain: {len(train_df):,}  |  Normal-train: {len(normal_train):,}")
+print(f"Test : {len(test_df):,}")
+
+# Impute
+continuous_imputer, discrete_imputer = impute(normal_train)
+apply_imputer(train_df, continuous_imputer, discrete_imputer)
+
+# Scale continuous values to be between 0 and 1
+scaler = scale(train_df)
+apply_scaler(test_df, scaler)
